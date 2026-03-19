2fb765b7cc8378c3fa7d33845bbe7359ba29c574
Okraedro <134737957+Okraedro@users.noreply.github.com>
2026-03-19 16:12:46 +0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,1 +1,19 @@
+from app import app, db
+from models import Book, Category
+from flask import render_template, request, redirect, url_for
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/books')
+def books():
+    sort_by = request.args.get('sort_by', 'title')
+    books = Book.query.order_by(getattr(Book, sort_by)).all()
+    categories = Category.query.all()
+    return render_template('books.html', books=books, categories=categories)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
