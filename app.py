7b27f8da3cc4fdd418ae501dad15754edeba49c9
Okraedro <134737957+Okraedro@users.noreply.github.com>
2026-03-19 16:11:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/bookstore'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
